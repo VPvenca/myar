043ce325f71b712d0986ab9fdf5cfcc294ddbfa2
VPvenca <v.pencik@email.cz>
2025-04-21 18:34:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ const urlsToCache = [
   '/img/augview.png', // Přidejte vaše logo
   // Přidejte další klíčové soubory, které tvoří základní UI
   // Např. soubory podmenu, pokud jsou důležité pro offline start:
-  '/assets/kyjov/kyjov.html',
+  '/kyjov.html',
   '/info.html',
   // POZOR: Velké soubory (videa, .mind) zde raději NECACHUJTE automaticky!
   // Ty se načtou ze sítě, když budou potřeba.
