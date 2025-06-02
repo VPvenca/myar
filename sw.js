3a742fb90ddf58d4694006571e4942a42a47486e
VPvenca <v.pencik@email.cz>
2025-06-02 14:04:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 console.log('Service Worker soubor načten.');
 // Service Worker soubor
 
-const CACHE_NAME = 'ar-app-cache-v55'; // Název cache (změňte 'v1' při aktualizaci cache)
+const CACHE_NAME = 'ar-app-cache-v56'; // Název cache (změňte 'v1' při aktualizaci cache)
 const urlsToCache = [
   
   '/index.html',
