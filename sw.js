e59545798b03e8fa2918330b38c0a4119802acb2
VPvenca <v.pencik@email.cz>
2025-06-16 13:39:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Optimalizovaný Service Worker pro iOS PWA
 console.log('Service Worker soubor načten.');
 
-const CACHE_NAME = 'ar-app-cache-v71'; // Zvyšte verzi po změnách
+const CACHE_NAME = 'ar-app-cache-v72'; // Zvyšte verzi po změnách
 const urlsToCache = [
   '/index.html',
   '/info.html', // Přidáno pro iOS
